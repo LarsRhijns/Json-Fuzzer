6f8953775d3d938bb961c287c02d1918e211f145
Melchior oudemans <m.w.m.oudemans@student.tudelft.nl>
2021-05-22 15:07:11 +0200

    Added faulty benchmark modules and corrected the first 6 faulty benchmarks

---
+++ faulty_benchmarks/src/FindSalary/FindSalaryDriver.java
@@ -0,0 +1,18 @@
+package FindSalary;
+
+import edu.berkeley.cs.jqf.fuzz.Fuzz;
+import edu.berkeley.cs.jqf.fuzz.JQF;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+@RunWith(JQF.class)
+
+public class FindSalaryDriver {
+
+@Fuzz
+    public void testFindSalary(String fileName) throws IOException {
+        System.out.println("edu.ucla.cs.bigfuzz.customarray.applicable.FindSalary.FindSalaryDriver::testFindSalary: "+fileName);
+        FindSalary analysis = new FindSalary();
+        analysis.FindSalary(fileName);
+    }
+}
