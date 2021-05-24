682fd71aae9246bf5b40ae5083635125d69d284b
LvKvA <l.vankoetsveldvanankeren@student.tudelft.nl>
2021-05-24 14:30:39 +0200

    Remove unused interface methods

--- fuzz/src/main/java/edu/ucla/cs/jqf/bigfuzz/BigFuzzMutation.java
+++ fuzz/src/main/java/edu/ucla/cs/jqf/bigfuzz/BigFuzzMutation.java
@@ -13,37 +13,16 @@ public interface BigFuzzMutation {
     public void mutate(String inputFile, String nextInputFile) throws IOException;
 
     /**
-     * mutate file based on index (support multiple data-specific mutations)
-     * @param inputFile, index
-     * @throws IOException
-     * */
-    public void mutateFile(String inputFile, int index) throws IOException;
-
-    /**
      * mutate on rows of an input file
      * @param rows
      */
     public void mutate(ArrayList<String> rows);
 
     /**
-     * Randomly duplicate some rows and then randomly insert into the input lines
-     * @param rows
-     */
-    public void randomDuplicateRows(ArrayList<String> rows);
-
-    /**
      * Randomly generate some rows and then randomly insert into the input lines
      * @param rows
      */
     public void randomGenerateRows(ArrayList<String> rows);
-
-
-
-    public void randomGenerateOneColumn(int columnID, int minV, int maxV, ArrayList<String> rows);
-
-    public void randomDuplicateOneColumn(int columnID, int intV, int maxV, ArrayList<String> rows);
-
-    public void improveOneColumn(int columnID, int intV, int maxV, ArrayList<String> rows);
 
     /**
      * write rows into csv txt file
