418e0faf15d1b1bdbd5113d483a8657f8ca2b723
Melchior oudemans <m.w.m.oudemans@student.tudelft.nl>
2021-06-01 14:36:28 +0200

    Fixed logging after merge

--- fuzz/src/main/java/edu/tud/cs/jqf/bigfuzzplus/BigFuzzPlusDriver.java
+++ fuzz/src/main/java/edu/tud/cs/jqf/bigfuzzplus/BigFuzzPlusDriver.java
@@ -10,7 +10,6 @@ import edu.tud.cs.jqf.bigfuzzplus.stackedMutation.StackedMutationEnum;
 import java.io.*;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
-import java.util.*;
 
 @SuppressWarnings("StringConcatenationInsideStringBufferAppend")
 public class BigFuzzPlusDriver {
@@ -23,6 +22,7 @@ public class BigFuzzPlusDriver {
     public static StringBuilder program_configuration = new StringBuilder();
     public static StringBuilder iteration_results = new StringBuilder();
     public static StringBuilder summarized_results = new StringBuilder();
+    public static BigFuzzPlusLog log = BigFuzzPlusLog.getInstance();
 
     /**
      * Run the BigFuzzPlus program with the following parameters for StackedMutation:
@@ -81,40 +81,21 @@ public class BigFuzzPlusDriver {
         // **************
 
         long programStartTime = System.currentTimeMillis();
-        File allOutputDir = new File("output");
-        File outputDir = new File(allOutputDir, "" + programStartTime);
+        File outputDir = new File("output/" + programStartTime);
 
-        program_configuration.append("Program started with the following parameters: ");
-        program_configuration.append("\n\tTest class: " + testClassName);
-        program_configuration.append("\n\tTest method: " + testMethodName);
-        program_configuration.append("\n\tTest method: " + mutationMethodClassName);
-        if (mutationMethodClassName.equalsIgnoreCase("StackedMutation")) {
-            program_configuration.append("\n\tTest stackedMutation method: " + stackedMutationMethod);
-            program_configuration.append("\n\tTest maximal stacked mutations: " + intMutationStackCount);
-        }
-        if (mutationMethodClassName.equals("SystematicMutation")) {
-            program_configuration.append("\n\tTest mutate columns: " + mutateColumns);
-            program_configuration.append("\n\tTest tree depth: " + mutationDepth);
-        }
-
-        program_configuration.append("\nOutput directory is set to: " + outputDir);
-        program_configuration.append("\nProgram is started at: " + programStartTime);
-
-        if (!allOutputDir.exists() && !allOutputDir.mkdir()) {
-            System.err.println("Something went wrong with making the output directory for this run: " + allOutputDir);
-            System.exit(0);
-        }
         if (!outputDir.mkdir()) {
             System.err.println("Something went wrong with making the output directory for this run: " + outputDir);
             System.exit(0);
         }
+        if (mutationMethodClassName.equalsIgnoreCase("stackedmutation")) {
+            log.logProgramArgumentsStackedMutation(testClassName, testMethodName, mutationMethodClassName, stackedMutationMethod, intMutationStackCount, outputDir, programStartTime);
+        } else if (mutationMethodClassName.equalsIgnoreCase("systematicmutation")) {
+            log.logProgramArgumentsSystematicMutation(testClassName, testMethodName, mutationMethodClassName, mutateColumns, mutationDepth, outputDir, programStartTime);
+        } else {
+            log.logProgramArguments(testClassName,testMethodName,mutationMethodClassName,outputDir,programStartTime);
+        }
+        log.printProgramArguments();
 
-        ArrayList<ArrayList<Integer>> uniqueFailureResults = new ArrayList<>();
-        ArrayList<ArrayList<String>> inputs = new ArrayList<>();
-        ArrayList<ArrayList<String>> methods = new ArrayList<>();
-        ArrayList<ArrayList<String>> columns = new ArrayList<>();
-        ArrayList<Long> durations = new ArrayList<>();
-        ArrayList<Integer> uniqueFailures = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             int atIteration = i + 1;
             System.out.println("******** START OF PROGRAM ITERATION: " + atIteration + "**********************");
@@ -131,7 +112,7 @@ public class BigFuzzPlusDriver {
                 // Set the provided input argument stackedMutationMethod in the guidance mutation
                 if (guidance.mutation instanceof StackedMutation) {
                     ((StackedMutation)guidance.mutation).setStackedMutationMethod(stackedMutationMethod);
-                    guidance.setMutationStackCount(intMutationStackCount);
+                    ((StackedMutation)guidance.mutation).setMutationStackCount(intMutationStackCount);
                 }
                 // Set the randomization seed to the program start time. Seed is passed to allow for custom seeds, independent of the program start time
                 guidance.setRandomizationSeed(programStartTime);
@@ -144,251 +125,15 @@ public class BigFuzzPlusDriver {
                 long endTime = System.currentTimeMillis();
 
                 // Evaluate the results
-//                evaluation(testClassName, testMethodName, file, maxTrials, maxDuration, iterationStartTime, endTime, guidance, atIteration);
-                writeToLists(guidance, maxTrials, inputs, uniqueFailureResults, methods, columns, uniqueFailures);
-                durations.add(endTime - iterationStartTime);
+                log.evaluation(testClassName, testMethodName, file, maxTrials, maxDuration, iterationStartTime, endTime, guidance, atIteration);
+                log.writeToLists(guidance, maxTrials);
+                log.addDuration(endTime - iterationStartTime);
                 System.out.println("************************* END OF PROGRAM ITERATION ************************");
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        summarizeProgramIterations(uniqueFailureResults, inputs, methods, columns, durations, uniqueFailures);
-        writeLogToFile(outputDir);
-    }
-
-    /**
-     * Write collected log in the variables log, summarized results and iteration results to a file in the output folder named log.txt.
-     * @param outputDir Directory where the log file should be written to
-     */
-    private static void writeLogToFile(File outputDir) {
-        File f_out = new File(outputDir + "/log.txt");
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(f_out);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.exit(0);
-        }
-
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
-        String output = program_configuration.append("\n\n").append(summarized_results).append("\n\n").append(iteration_results).toString();
-
-        try {
-            bw.write(output);
-            bw.close();
-            fos.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Convert collected data lists to a readable string in summarized_results
-     * @param uniqueFailureResults List of unique failures per program iteration
-     * @param inputs    List of sequential mutated inputs per program iteration
-     * @param methods   List of summed HighOrderMutation methods applied per iteration
-     * @param columns   List of count per column how many times mutation was applied to said column
-     * @param durations List of iteration durations
-     */
-    @SuppressWarnings("DuplicatedCode")
-    private static void summarizeProgramIterations(ArrayList<ArrayList<Integer>> uniqueFailureResults, ArrayList<ArrayList<String>> inputs, ArrayList<ArrayList<String>> methods,
-                                                   ArrayList<ArrayList<String>> columns, ArrayList<Long> durations, ArrayList<Integer> uniqueFailures) {
-
-        summarized_results.append("********* PROGRAM SUMMARY **********");
-        // --------------- UNIQUE FAILURES --------------
-        summarized_results.append("\nCUMULATIVE UNIQUE FAILURE PER TEST PER ITERATION");
-        for (int i = 0; i < uniqueFailureResults.size(); i++) {
-            summarized_results.append("\nRun " + (i + 1) + ": " + uniqueFailureResults.get(i));
-        }
-
-        // --------------- INPUTS --------------
-        summarized_results.append("\n\nAPPLIED MUTATIONS PER ITERATION");
-        for (int i = 0; i < inputs.size(); i++) {
-            summarized_results.append("\nRun " + (i + 1) + " [");
-            for (int j = 0; j < inputs.get(i).size(); j++) {
-                if (j != 0) {
-                    summarized_results.append(", ");
-                }
-                summarized_results.append("\"" + inputs.get(i).get(j) + "\"");
-            }
-            summarized_results.append("]");
-        }
-
-        // --------------- MUTATION COUNTER --------------
-        summarized_results.append("\n\n MUTATED INPUTS PER ITERATION");
-        for (int i = 0; i < methods.size(); i++) {
-            summarized_results.append("\nRun " + (i + 1) + ": [");
-            for (int j = 0; j < methods.get(i).size(); j++) {
-                if (j != 0) {
-                    summarized_results.append(", ");
-                }
-                summarized_results.append("(" + methods.get(i).get(j) + ")");
-            }
-            summarized_results.append("]");
-        }
-
-        // --------------- COLUMN COUNTER --------------
-        summarized_results.append("\n\n MUTATIONS APPLIED ON COLUMN PER ITERATION");
-        for (int i = 0; i < columns.size(); i++) {
-            summarized_results.append("\nRun " + (i + 1) + ": [");
-            for (int j = 0; j < columns.get(i).size(); j++) {
-                if (j != 0) {
-                    summarized_results.append(", ");
-                }
-                summarized_results.append("(" + columns.get(i).get(j) + ")");
-            }
-            summarized_results.append("]");
-        }
-
-        // --------------- DURATION --------------
-        summarized_results.append("\n\n DURATION PER ITERATION");
-        summarized_results.append("\ndurations: " + durations);
-        for (int i = 0; i < durations.size(); i++) {
-            summarized_results.append("\nRun " + (i + 1) + ": " + durations.get(i) + " ms ");
-        }
-
-        // --------------- UNIQUE FAILURES --------------
-        summarized_results.append("\n\n UNIQUE FAILURES PER RUN");
-        for (int i = 0; i < uniqueFailures.size(); i++) {
-            summarized_results.append("\nRun " + (i + 1) + ": " + uniqueFailures.get(i));
-        }
-
-        System.out.println(summarized_results);
-    }
-
-    /**
-     * Transforms data in guidance to required lists.
-     * @param guidance  guidance class which contains all data
-     * @param maxTrials maximal amount of trials (configuration)
-     * @param inputs list of inputs passed to the program that is being tested
-     * @param uniqueFailureResults list of unique failures
-     * @param methods list of methods applied
-     * @param columns list of count how many times mutation was applied per column
-     */
-    private static void writeToLists(BigFuzzPlusGuidance guidance, Long maxTrials, ArrayList<ArrayList<String>> inputs, ArrayList<ArrayList<Integer>> uniqueFailureResults, ArrayList<ArrayList<String>> methods, ArrayList<ArrayList<String>> columns, ArrayList<Integer> uniqueFailures) {
-        // Unique failure results
-        int cumulative = 0;
-        ArrayList<Integer> runFoundUniqueFailureCumulative = new ArrayList<>();
-        for (long j = 0; j < maxTrials; j++) {
-            if (guidance.uniqueFailureRuns.contains(j))
-                cumulative++;
-            runFoundUniqueFailureCumulative.add(cumulative);
-        }
-        // Methods and columns
-        if(guidance.mutation instanceof StackedMutation) {
-            ArrayList<String> methodStringList = new ArrayList<>();
-            ArrayList<String> columnStringList = new ArrayList<>();
-            combineUsedMethodsAndColumns(methodStringList, columnStringList, guidance);
-            methods.add(methodStringList);
-            columns.add(columnStringList);
-        }
-
-        inputs.add(guidance.inputs);
-        uniqueFailureResults.add(runFoundUniqueFailureCumulative);
-        uniqueFailures.add(cumulative);
-    }
-
-    @SuppressWarnings("rawtypes")
-    private static void combineUsedMethodsAndColumns(ArrayList<String> methodStringList, ArrayList<String> columnStringList, BigFuzzPlusGuidance guidance) {
-        ArrayList<HighOrderMutation.HighOrderMutationMethod> methodTracker = ((StackedMutation) guidance.mutation).getMutationMethodTracker();
-        ArrayList<Integer> columnTracker = ((StackedMutation) guidance.mutation).getMutationColumnTracker();
-
-        HashMap<HighOrderMutation.HighOrderMutationMethod, Integer> methodMap = new HashMap<>();
-        HashMap<Integer, Integer> columnMap = new HashMap<>();
-        for (int i = 0; i < methodTracker.size(); i++) {
-            HighOrderMutation.HighOrderMutationMethod method = methodTracker.get(i);
-            int column = columnTracker.get(i);
-            if (methodMap.containsKey(method)) {
-                methodMap.put(method, methodMap.get(method) + 1);
-            } else {
-                methodMap.put(method, 1);
-            }
-            if (columnMap.containsKey(column)) {
-                columnMap.put(column, columnMap.get(column) + 1);
-            } else {
-                columnMap.put(column, 1);
-            }
-        }
-        for (Map.Entry e : methodMap.entrySet()) {
-            methodStringList.add(e.getKey() + ": " + e.getValue());
-        }
-
-        for (Map.Entry e : columnMap.entrySet()) {
-            columnStringList.add(e.getKey() + ": " + e.getValue());
-        }
-    }
-
-    /**
-     * Prints the configuration and the results from the run to the Terminal.
-     *
-     * @param testClassName  Class name which is being tested
-     * @param testMethodName Test method name which is used to perform the test
-     * @param file           Input file for the testing
-     * @param maxTrials      maximal amount of trials configuration
-     * @param duration       maximal duration of the trials configuration
-     * @param startTime      start time of the program
-     * @param endTime        end time of the program
-     * @param guidance       guidance class which is used to perform the BigFuzz testing
-     * @param atIteration    Counter indicating for which iteration this evaluation is.
-     */
-    private static void evaluation(String testClassName, String testMethodName, String file, Long maxTrials, Duration duration, long startTime, long endTime, BigFuzzPlusGuidance guidance, int atIteration) {
-        StringBuilder e_log = new StringBuilder();
-        // Print configuration
-        e_log.append("*** TEST " + atIteration + " LOG ***");
-        e_log.append("\n---CONFIGURATION---");
-        e_log.append("\nFiles used..." + "\n\tconfig:\t\t" + file + "\n\ttestClass:\t" + testClassName + "\n\ttestMethod:\t" + testMethodName);
-        e_log.append("Max trials: " + maxTrials);
-        e_log.append("Max duration: " + duration.toMillis() + "ms");
-
-        e_log.append("\n---REPRODUCIBILITY---");
-        if (guidance.mutation instanceof StackedMutation) {
-            e_log.append("\n\tRandomization seed: " + ((StackedMutation) guidance.mutation).getRandomizationSeed());
-        }
-        e_log.append("\n\tMutated inputs: [");
-        for (int i = 0; i < guidance.inputs.size(); i++) {
-            if (i != 0) {
-                e_log.append(", ");
-            }
-            e_log.append("\"" + guidance.inputs.get(i) + "\"");
-        }
-        e_log.append("]");
-
-        // Print results
-        e_log.append("\n---RESULTS---");
-
-        // Failures
-        e_log.append("\n\tTotal Failures: " + guidance.totalFailures);
-        e_log.append("\n\tUnique Failures: " + guidance.uniqueFailures.size());
-        e_log.append("\n\tUnique Failures found at: " + guidance.uniqueFailureRuns);
-        List<Boolean> runFoundUniqueFailure = new ArrayList<>();
-        int cumulative = 0;
-        List<Integer> runFoundUniqueFailureCumulative = new ArrayList<>();
-        for (long i = 0; i < maxTrials; i++) {
-            runFoundUniqueFailure.add(guidance.uniqueFailureRuns.contains(i));
-            if (guidance.uniqueFailureRuns.contains(i))
-                cumulative++;
-            runFoundUniqueFailureCumulative.add(cumulative);
-        }
-        e_log.append("\n\tUnique Failure found per run: " + runFoundUniqueFailure);
-        e_log.append("\n\tUnique Failure found per run: " + runFoundUniqueFailureCumulative);
-
-        // Run time
-        long totalDuration = endTime - startTime;
-        if (guidance.numTrials != maxTrials) {
-            e_log.append("Could not complete all trials in the given duration.");
-        }
-        e_log.append("\n\tTotal run time：" + totalDuration + "ms");
-        e_log.append("\n\tTests run: " + guidance.numTrials);
-        e_log.append("\n\tAverage test run time: " + (float) totalDuration / guidance.numTrials + "ms");
-
-        // Coverage
-        int totalCov = guidance.totalCoverage.getNonZeroCount();
-        int validCov = guidance.validCoverage.getNonZeroCount();
-        e_log.append("\n\tTotal coverage: " + totalCov);
-        e_log.append("\n\tValid coverage: " + validCov);
-
-        e_log.append("\n\tPercent valid coverage: " + (float) validCov / totalCov * 100 + "%");
-        System.out.println(e_log);
-        iteration_results.append(e_log);
+        log.summarizeProgramIterations();
+        log.writeLogToFile(outputDir);
     }
 }
