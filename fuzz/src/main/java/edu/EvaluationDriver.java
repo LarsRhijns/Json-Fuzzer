542847be05669085cb49d7839614ed42bdeccc63
Melchior oudemans <m.w.m.oudemans@student.tudelft.nl>
2021-06-03 13:24:02 +0200

    Implemented the evaluation tool to run the bigfuzzplusdriver multiple times

--- fuzz/src/main/java/edu/EvaluationDriver.java
+++ fuzz/src/main/java/edu/EvaluationDriver.java
@@ -1,21 +1,22 @@
 package edu;
 
 import edu.tud.cs.jqf.bigfuzzplus.BigFuzzPlusDriver;
+import edu.tud.cs.jqf.bigfuzzplus.BigFuzzPlusLog;
 
 public class EvaluationDriver {
 
     public static void main (String[] args) {
-        String testClass = "SalaryAnalysisDriver";
-        String testMethod = "testSalaryAnalysis";
+        String testClass = "PropertyDriver";
+        String testMethod = "testProperty";
         String mutationMethod = "StackedMutation";
         String numTrials = "5000";
         String stackedmethod = "3";
-        String maxStack = "3";
-        String[] args2 = {testClass,testMethod,mutationMethod,numTrials,stackedmethod,maxStack};
-        BigFuzzPlusDriver.main(args2);
-
-
-
-
+        String[] maxStack = {"5","7","10"};
+        String[] args2 = {testClass,testMethod,mutationMethod,numTrials,stackedmethod,"1"};
+        for (int i = 0; i < 3; i++) {
+            args2[5] = maxStack[i];
+            BigFuzzPlusDriver.main(args2);
+            BigFuzzPlusLog.resetInstance();
+        }
     }
 }
