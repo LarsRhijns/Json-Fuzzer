e84d2bc4cc341137ce3130249084563640117a3c
Bo van den Berg <b.vandenberg-6@student.tudelft.nl>
2021-06-26 13:50:08 +0200

    Requested changes
    - Use only 1 selection
    - Update gitignore

--- fuzz/src/main/java/edu/tud/cs/jqf/bigfuzzplus/BigFuzzPlusDriver.java
+++ fuzz/src/main/java/edu/tud/cs/jqf/bigfuzzplus/BigFuzzPlusDriver.java
@@ -31,7 +31,7 @@ public class BigFuzzPlusDriver {
     public static boolean SAVE_UNIQUE_FAILURES = true;
 	public static int NUMBER_OF_ITERATIONS = 10;
 	public static Duration maxDuration = null; // example: Duration.of(30, ChronoUnit.MINUTES);
-	public static List<SelectionMethod> selections = new ArrayList<>(Arrays.asList(FULLY_BOOSTED_GREY_BOX));
+	public static SelectionMethod selection = FULLY_BOOSTED_GREY_BOX;
 
     /**
      * Run the BigFuzzPlus program with the following parameters for StackedMutation:
@@ -77,140 +77,138 @@ public class BigFuzzPlusDriver {
 		long programStartTime = System.currentTimeMillis();
 
 	    BigFuzzPlusLog log = new BigFuzzPlusLog();
-		for (SelectionMethod selection : selections) {
-			BigFuzzPlusLog.resetInstance();
+		BigFuzzPlusLog.resetInstance();
 
-			String selectionMethodString;
-			if (selection == SelectionMethod.FULLY_BOOSTED_GREY_BOX) {
-				selectionMethodString = "FBGB";
-			} else if (selection == SelectionMethod.HALF_BOOSTED_GREY_BOX) {
-				selectionMethodString = "HBGB";
-			} else if (selection == GREY_BOX) {
-				selectionMethodString = "GB";
-			} else {
-				selectionMethodString = "BB";
-			}
-
-			File allOutputDir = new File("fuzz-results");
-			File outputDir = new File(allOutputDir, "" + programStartTime + " " + testClassName +
-					" " + mutationMethodClassName + " " +
-					" " + selectionMethodString + " " + NUMBER_OF_ITERATIONS + "x" + maxTrials);
-
-			if (!allOutputDir.exists() && !allOutputDir.mkdir()) {
-				System.err.println("Something went wrong with making the output directory for this run: " + allOutputDir);
-				System.exit(0);
-			}
-			if (!outputDir.mkdir()) {
-				System.err.println("Something went wrong with making the output directory for this run: " + outputDir);
-				System.exit(0);
-			}
-			if (CLEAR_ALL_PREVIOUS_RESULTS_ON_START && allOutputDir.isDirectory()) {
-				try {
-					FileUtils.cleanDirectory(allOutputDir);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-
-			StackedMutationEnum.StackedMutationMethod stackedMutationMethod = StackedMutationEnum.StackedMutationMethod.Disabled;
-			int intMutationStackCount = 0;
-			log.logProgramArguments(testClassName, testMethodName, mutationMethodClassName, outputDir, programStartTime);
-			if (mutationMethodClassName.equalsIgnoreCase("stackedmutation")) {
-				int intStackedMutationMethod = args.length > 4 ? Integer.parseInt(args[4]) : 0;
-				// This variable is used for the stackedMutationMethod: Smart_mutate
-				// If the selected stackedMutationMethod is smart_mutate and this argument is not given, default is set to 2. If smart_mutate is not selected, set to 0
-				stackedMutationMethod = StackedMutationEnum.intToStackedMutationMethod(intStackedMutationMethod);
-				intMutationStackCount = args.length > 5 ? Integer.parseInt(args[5]) : stackedMutationMethod == StackedMutationEnum.StackedMutationMethod.Smart_stack ? 2 : 0;
-				log.logProgramArgumentsStackedMutation(stackedMutationMethod, intMutationStackCount);
-			}
-			else if (mutationMethodClassName.equalsIgnoreCase("systematicmutation")) {
-				if (args.length > 4) {
-					MUTATE_COLUMNS = Boolean.parseBoolean(args[4]);
-				} if (args.length > 5) {
-					MUTATION_DEPTH = Integer.parseInt(args[5]);
-				}
-				log.logProgramArgumentsSystematicMutation(MUTATE_COLUMNS, MUTATION_DEPTH);
-			}
-			else if (mutationMethodClassName.equalsIgnoreCase("random")) {
-				MUTATE_RANDOM = true;
-				System.out.println("Mutating randomly");
-				log.logProgramArgumentsSystematicMutation(MUTATE_COLUMNS, MUTATION_DEPTH);
-			}
-
-			String file;
-			switch (testClassName) {
-				case "WordCountDriver":
-				case "WordCountNewDriver":
-					file = "dataset/conf_wordcount";
-					break;
-				case "CommuteTypeDriver":
-					file = "dataset/commutetype";
-					break;
-				case "ExternalUDFDriver":
-					file = "dataset/conf_externaludf";
-					break;
-				case "FindSalaryDriver":
-					file = "dataset/conf_findsalary";
-					break;
-				case "StudentGradesDriver":
-					file = "dataset/conf_studentgrades";
-					break;
-				case "MovieRatingDriver":
-					file = "dataset/conf_movierating";
-					break;
-				case "SalaryAnalysisDriver":
-					file = "dataset/conf_salary";
-					break;
-				case "PropertyDriver":
-					file = "dataset/conf_property";
-					break;
-				case "BranchMarkDriver":
-					file = "dataset/conf_branchmark";
-					break;
-				default:
-					file = "dataset/conf";
-			}
-
-			log.printProgramArguments();
-			System.out.println();
-
-			System.out.println("************ PROGRAM RUNNING ************");
-			for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) {
-				int atIteration = i + 1;
-				System.out.println("*** ITERATION " + atIteration + " - " + selectionMethodString);
-
-				long iterationStartTime = System.currentTimeMillis();
-				String iterationOutputDir = outputDir + "/Test" + atIteration;
-
-				try {
-					File itOutputDir = new File(iterationOutputDir);
-					BigFuzzPlusGuidance guidance = new BigFuzzPlusGuidance("Test" + atIteration, file, maxTrials, maxDuration, itOutputDir, mutationMethodClassName, selection);
-
-					// Set the provided input argument stackedMutationMethod in the guidance mutation
-					if(guidance.mutation instanceof StackedMutation) {
-						((StackedMutation) guidance.mutation).setStackedMutationMethod(stackedMutationMethod);
-						((StackedMutation) guidance.mutation).setMutationStackCount(intMutationStackCount);
-					}
-
-					// Set the randomization seed to the program start time. Seed is passed to allow for custom seeds, independent of the program start time
-					guidance.setRandomizationSeed(iterationStartTime);
-
-					// Set the test class name in the guidance for the failure tracking
-					guidance.setTestClassName(testClassName);
-
-					// Run the Junit test
-					GuidedFuzzing.run(testClassName, testMethodName, guidance, System.out);
-					long endTime = System.currentTimeMillis();
-
-					// Evaluate the results
-					log.writeToLists(guidance, maxTrials);
-					log.addDuration(endTime - iterationStartTime);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-			log.summarizeProgramIterations();
-			log.writeLogToFile(outputDir);
+		String selectionMethodString;
+		if (selection == SelectionMethod.FULLY_BOOSTED_GREY_BOX) {
+			selectionMethodString = "FBGB";
+		} else if (selection == SelectionMethod.HALF_BOOSTED_GREY_BOX) {
+			selectionMethodString = "HBGB";
+		} else if (selection == GREY_BOX) {
+			selectionMethodString = "GB";
+		} else {
+			selectionMethodString = "BB";
 		}
+
+		File allOutputDir = new File("fuzz-results");
+		File outputDir = new File(allOutputDir, "" + programStartTime + " " + testClassName +
+				" " + mutationMethodClassName + " " +
+				" " + selectionMethodString + " " + NUMBER_OF_ITERATIONS + "x" + maxTrials);
+
+		if (!allOutputDir.exists() && !allOutputDir.mkdir()) {
+			System.err.println("Something went wrong with making the output directory for this run: " + allOutputDir);
+			System.exit(0);
+		}
+		if (!outputDir.mkdir()) {
+			System.err.println("Something went wrong with making the output directory for this run: " + outputDir);
+			System.exit(0);
+		}
+		if (CLEAR_ALL_PREVIOUS_RESULTS_ON_START && allOutputDir.isDirectory()) {
+			try {
+				FileUtils.cleanDirectory(allOutputDir);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		StackedMutationEnum.StackedMutationMethod stackedMutationMethod = StackedMutationEnum.StackedMutationMethod.Disabled;
+		int intMutationStackCount = 0;
+		log.logProgramArguments(testClassName, testMethodName, mutationMethodClassName, outputDir, programStartTime);
+		if (mutationMethodClassName.equalsIgnoreCase("stackedmutation")) {
+			int intStackedMutationMethod = args.length > 4 ? Integer.parseInt(args[4]) : 0;
+			// This variable is used for the stackedMutationMethod: Smart_mutate
+			// If the selected stackedMutationMethod is smart_mutate and this argument is not given, default is set to 2. If smart_mutate is not selected, set to 0
+			stackedMutationMethod = StackedMutationEnum.intToStackedMutationMethod(intStackedMutationMethod);
+			intMutationStackCount = args.length > 5 ? Integer.parseInt(args[5]) : stackedMutationMethod == StackedMutationEnum.StackedMutationMethod.Smart_stack ? 2 : 0;
+			log.logProgramArgumentsStackedMutation(stackedMutationMethod, intMutationStackCount);
+		}
+		else if (mutationMethodClassName.equalsIgnoreCase("systematicmutation")) {
+			if (args.length > 4) {
+				MUTATE_COLUMNS = Boolean.parseBoolean(args[4]);
+			} if (args.length > 5) {
+				MUTATION_DEPTH = Integer.parseInt(args[5]);
+			}
+			log.logProgramArgumentsSystematicMutation(MUTATE_COLUMNS, MUTATION_DEPTH);
+		}
+		else if (mutationMethodClassName.equalsIgnoreCase("random")) {
+			MUTATE_RANDOM = true;
+			System.out.println("Mutating randomly");
+			log.logProgramArgumentsSystematicMutation(MUTATE_COLUMNS, MUTATION_DEPTH);
+		}
+
+		String file;
+		switch (testClassName) {
+			case "WordCountDriver":
+			case "WordCountNewDriver":
+				file = "dataset/conf_wordcount";
+				break;
+			case "CommuteTypeDriver":
+				file = "dataset/commutetype";
+				break;
+			case "ExternalUDFDriver":
+				file = "dataset/conf_externaludf";
+				break;
+			case "FindSalaryDriver":
+				file = "dataset/conf_findsalary";
+				break;
+			case "StudentGradesDriver":
+				file = "dataset/conf_studentgrades";
+				break;
+			case "MovieRatingDriver":
+				file = "dataset/conf_movierating";
+				break;
+			case "SalaryAnalysisDriver":
+				file = "dataset/conf_salary";
+				break;
+			case "PropertyDriver":
+				file = "dataset/conf_property";
+				break;
+			case "BranchMarkDriver":
+				file = "dataset/conf_branchmark";
+				break;
+			default:
+				file = "dataset/conf";
+		}
+
+		log.printProgramArguments();
+		System.out.println();
+
+		System.out.println("************ PROGRAM RUNNING ************");
+		for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) {
+			int atIteration = i + 1;
+			System.out.println("*** ITERATION " + atIteration);
+
+			long iterationStartTime = System.currentTimeMillis();
+			String iterationOutputDir = outputDir + "/Test" + atIteration;
+
+			try {
+				File itOutputDir = new File(iterationOutputDir);
+				BigFuzzPlusGuidance guidance = new BigFuzzPlusGuidance("Test" + atIteration, file, maxTrials, maxDuration, itOutputDir, mutationMethodClassName, selection);
+
+				// Set the provided input argument stackedMutationMethod in the guidance mutation
+				if(guidance.mutation instanceof StackedMutation) {
+					((StackedMutation) guidance.mutation).setStackedMutationMethod(stackedMutationMethod);
+					((StackedMutation) guidance.mutation).setMutationStackCount(intMutationStackCount);
+				}
+
+				// Set the randomization seed to the program start time. Seed is passed to allow for custom seeds, independent of the program start time
+				guidance.setRandomizationSeed(iterationStartTime);
+
+				// Set the test class name in the guidance for the failure tracking
+				guidance.setTestClassName(testClassName);
+
+				// Run the Junit test
+				GuidedFuzzing.run(testClassName, testMethodName, guidance, System.out);
+				long endTime = System.currentTimeMillis();
+
+				// Evaluate the results
+				log.writeToLists(guidance, maxTrials);
+				log.addDuration(endTime - iterationStartTime);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		log.summarizeProgramIterations();
+		log.writeLogToFile(outputDir);
     }
 }
